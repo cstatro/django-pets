4f4a605dc04df541a3fe65d31b39d9698fc6f26e
Colin <cstatro@gmail.com>
2019-11-19 18:41:27 -0500

    done

--- pets/dogs/tests.py
+++ pets/dogs/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
-
+from .serializers import DogSerializer
+from .models import Dog
 # Create your tests here.
+
+
+class DogTest(TestCase):
+    def setUp(self):
+        dog_one = dict(name="jimmy", favorite_food="kibble")
+        serializer = DogSerializer()
+        serializer.create(dog_one)
+
+    def test_dog_one(self):
+        """finds jimmy as the first dog"""
+        name = Dog.objects.first().name
+        self.assertEqual(name, "jimmy")
